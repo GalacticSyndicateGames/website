5e7190b7b89a819174701ccb18d6e7f4344c3cd1
Balits Peter <97095806+balits@users.noreply.github.com>
2023-07-22 15:34:23 +0200

    prepare(gh-pages): testing new workflow, and husky

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const isDev = process.argv.includes('dev');
+
 /** @type {import('mdsevx').MdsvexOptions} */
 const markdownConfig = {
 	extensions: ['.md'],
@@ -20,7 +22,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			//This should always match the name of the repository
+			base: isDev? '' : process.env.BASE_PATH
+		}
 	}
 };
 
