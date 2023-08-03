cdd2f7948fcab441095ed7c742911252cd27de30
balits <balits.peter.marton@gmail.com>
2023-08-03 11:41:43 +0200

    fix(reload not working): base path not needed when using custom domain

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const isDev = process.argv.includes('dev');
-
 /** @type {import('mdsevx').MdsvexOptions} */
 const markdownConfig = {
 	extensions: ['.md'],
@@ -22,12 +20,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-			//This should always match the name of the repository
-			//base: isDev ? '' : '/website'
-			base: ''
-		}
+		adapter: adapter()
 	}
 };
 
