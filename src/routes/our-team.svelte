e91e96f62b579b9a534bca8c4c6ff6b876922dad
balits <balits.peter.marton@gmail.com>
2023-08-04 14:36:31 +0200

    add(bio of alex and reka)

--- src/routes/our-team.svelte
+++ src/routes/our-team.svelte
@@ -37,12 +37,6 @@
       description: 'Betti has a remarkable ability to identify exceptional talents. She has worked with global companies including Cloudera and GitLab to assemble winning teams that thrive on creative problem solving. Her emphasis on fostering a culture of innovation and driving efficiency has made a significant impact on the success of the organizations she has worked with.'
 
     },
-    {
-      name: 'Andras',
-      role: 'Intern',
-      imgSrc: '/images/team/andras.png',
-      description: 'Ever since his childhood Andras was always entertained by the worlds of books, movies, and video games. He also enjoy creating and building things, figuring out solutions to mind-bending problems. Game development has everything Andras love: fantasy, creativity, ingenuity. Andras mainly enjoy working on Gameplay, that is where he is most experienced.'
-    },
 		{
 			name: 'Ishai',
 			role: 'Advisor',
@@ -50,6 +44,24 @@
 			description:
 				'On an advisory level Ishai brings to us his extensive work experience in the tech industry. Ishai was the CEO & Founder of Growth Streak, Business Performance at Proteus Games, Business Growth Advisor at Lightricks, CPO at JOB TODAY, Senior Director, Product Management & Business Performance and Director, Product Management & Business Performance at King, Lead Product Manager, Senior Product Manager and Product Manager at Electronic Arts (EA), Co-Founder/Product Manager/Android Developer at CraveEat Mobile, Business Development Manager / Economy Manager at P-Kama, and Business Development Manager at Shidonni.. He also held a Strategic Review and Marketing Intelligence Project at Geoservices.  Ishai Shamir received their MBA from HEC Paris in 2009, specializing in Entrepreneurship. Ishai also obtained a Bachelor of Arts (BA) in Psychology and a Bachelor of Laws (LL.B.) from Tel Aviv University between 2002 and 2006.'
 		},
+    {
+      name: 'Andras',
+      role: 'Engineer',
+      imgSrc: '/images/team/andras.png',
+      description: 'Ever since his childhood Andras was always entertained by the worlds of books, movies, and video games. He also enjoy creating and building things, figuring out solutions to mind-bending problems. Game development has everything Andras love: fantasy, creativity, ingenuity. Andras mainly enjoy working on Gameplay, that is where he is most experienced.'
+    },
+    {
+      name: 'Reka',
+      role: 'Designer',
+      imgSrc: '/images/team/reka.png',
+      description: 'Réka is our designer with a passion for creating visually stunning and user-friendly digital experiences. Réka has loved the world of art since she was young. She studied engineering at university, where she realized that the combination of the two was the perfect profession for her.'
+    },
+    {
+      name: 'Alex',
+      role: 'Engineer',
+      imgSrc: '/images/team/alex.png',
+      description: 'Alex is a passionate software engineer, striving for efficiency in his work. His journey began in high school, where he learned programming, and it has led him to experiment with all kinds of coding. He finds joy in developing optimized solutions for backend services and internal tools.'
+    }
 	];
 </script>
 
