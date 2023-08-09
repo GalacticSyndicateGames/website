c06c9b0faa1f4a978899c1b7f368efffc1b7c1fb
balits <balits.peter.marton@gmail.com>
2023-08-09 14:11:23 +0200

    fix(footer): items no longer overlap

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	import { base } from '$app/paths';
-	import Logo from './logo.svelte';
-
 	const socials = [
 		/* {
 			href: 'https://www.facebook.com',
@@ -22,21 +19,21 @@
 </script>
 
 <footer
-	class="relative w-ful flex h-24 md:h-44 xl:h-72 w-full justify-center items-center gap-4 xl:gap-40 overflow-x-hidden bg-navy px-wrapper"
+	class="relative flex h-20 md:h-36 xl:h-44 w-full justify-between items-center gap-8 xl:gap-40 overflow-x-hidden bg-navy "
 >
-  <div class="left-6 absolute w-fit h-full inline-flex items-center justify-center">
-    <Logo/>
-  </div>
+    <img class="pl-wrapper w-40 sm:w-48 md:w-60 lg:w-64 xl:w-80 " src="/svgs/logo.svg" alt="Logo of the company">
+  <!-- <div class="left-6 absolute w-fit h-full inline-flex items-center justify-center">
+  </div> -->
 
 	<nav class="w-fit  xl:space-y-4 flex flex-col">
-		<h2 class="font-aldrich text-sm md:text-2xl xl:text-5xl uppercase">
+		<h2 class="font-aldrich text-sm md:text-2xl xl:text-4xl uppercase">
 			Social&nbsp;media
 		</h2>
 		<ul class="flex w-full justify-center gap-x-2 xl:gap-x-16">
 			{#each socials as s}
 				<li class="">
 					<a href={s.href} target="_blank" >
-						<img src="{base}{s.iconPath}" alt={s.title}  class="aspect-square w-6 h-6  md:w-12 md:h-12  xl:w-20 xl:h-20"/>
+						<img src="{s.iconPath}" alt={s.title}  class="aspect-square w-6 h-6  md:w-12 md:h-12  xl:w-16 xl:h-16"/>
 					</a>
 				</li>
 			{/each}
@@ -44,8 +41,8 @@
 	</nav>
 
 	 <img
-		class="-right-[10%] absolute h-full " 
-		src="{base}/svgs/layout/footer-planet.svg"
+		class="sm:translate-x-[20%] h-full " 
+		src="/svgs/layout/footer-planet.svg"
 		alt="A Pink planet floating away"
 	/> 
 </footer>
--- src/lib/components/logo.svelte
+++ src/lib/components/logo.svelte
@@ -1,5 +1,1 @@
-<script lang="ts">
-	import { base } from "$app/paths";
-</script>
-
-<img class="w-44 md:w-80 lg:w-96 xl:w-[34rem]" src="{base}/svgs/logo.svg" alt="Logo of the company">
+<img class="w-44 md:w-80 lg:w-96 xl:w-[34rem]" src="/svgs/logo.svg" alt="Logo of the company">
