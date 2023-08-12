76dcba2513e7be6084d300255764ed61c21e2a63
balits <balits.peter.marton@gmail.com>
2023-08-12 13:33:14 +0200

    update(/team): move 'our team' to its own route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,67 @@
 <script lang="ts">
-	import Hero from './hero.svelte';
-	import OurTeam from './our-team.svelte';
+	import Logo from '$lib/components/logo.svelte';
+
+	let showText = false;
 </script>
 
 <svelte:head>
 	<title>Galactic Syndicate</title>
 </svelte:head>
 
+<!-- <div
+	class=" bg-[url('')] bg-black bg-contain bg-no-repeat md:bg-[url('/svgs/layout/home.svg')] md:bg-auto 4xl:bg-contain"
+> -->
 <div
-	class=" bg-[url('/svgs/layout/home-bg-mobile.svg')] bg-black bg-contain bg-no-repeat md:bg-[url('/svgs/layout/home-bg.svg')] md:bg-auto 4xl:bg-contain"
+	class="xbg-contain h-screen bg-black bg-[url('/svgs/layout/home-bg-mobile.svg')] bg-contain bg-right-top bg-no-repeat md:bg-[url('/svgs/layout/home-bg.svg')] xl:bg-cover"
 >
-	<Hero />
-	<OurTeam />
+	<section
+		class="p-wrapper pt-headerHeight relative flex w-full flex-col items-start justify-between gap-36 overflow-x-hidden pb-4 text-white xl:h-full xl:gap-0"
+	>
+		<div class="inline-block w-fit text-justify after:content-['']">
+			<Logo />
+			<p
+				class="-mt-1 ml-1 font-anonymous text-sm font-semibold capitalize drop-shadow-md md:-mt-3 md:ml-2 md:text-2xl lg:ml-3 xl:-mt-4 xl:ml-4 xl:text-4xl"
+			>
+				Mobile Game Development
+			</p>
+		</div>
+
+		<div class="z-10 flex w-full flex-col items-start justify-between gap-10">
+			<h1
+				class="font-anonymous text-3xl font-semibold capitalize md:text-3xl xl:text-6xl"
+			>
+				Partnering<br />
+				With Intent
+			</h1>
+
+			<button
+				on:click={() => (showText = !showText)}
+				class="inline-block rounded-full bg-pistachio px-6 pb-1 pt-2 font-aldrich font-medium uppercase text-white transition duration-150 ease-in-out hover:bg-pistachio/90 focus:bg-pistachio/90 active:bg-pistachio/80 md:text-xl xl:text-3xl"
+			>
+				Learn more
+			</button>
+
+			<p
+				class="{showText
+					? 'translate-y-0 opacity-100'
+					: '-translate-y-4 opacity-0'} inline-flex w-full max-w-sm flex-col font-anonymous text-sm leading-none drop-shadow-md transition duration-300 md:max-w-lg md:text-xl"
+			>
+				<span class="">
+					Our Quality Execution Focusing On Your KPIs <br />
+					Our Experience Focused To Deliver On Your KPIs <br />
+					Your KPIs, Our Experience: Quality Execution
+				</span>
+				<span class="mt-4 xl:mt-6">
+					We can make your revenue enabling project a reality when your
+					resources are tight
+				</span>
+			</p>
+		</div>
+	</section>
 </div>
+
+<style>
+	section {
+		height: 100svh;
+	}
+</style>
---
+++ src/routes/team/+page.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+	import type { Member } from '$lib/components/member-card.svelte';
+	import MemberCard from '$lib/components/member-card.svelte';
+
+	const team: Array<Member> = [
+		{
+			name: 'Balazs',
+			role: 'Founder, CEO',
+			imgSrc: '/images/team/balazs.png',
+			description:
+				'Balazs has a knack for building and running high-performing product engineering teams. In his roles at various small startups and big names like Cloudera, Formlabs, Prezi and Nokia he saw that people who worked around him made things happen. Based in Budapest but with his heart in California, he brings a cool mix of innovation and know-how to the table.'
+		},
+		{
+			name: 'Balazs K. (BK)',
+			role: 'Software Engineer',
+			imgSrc: '/images/team/balazs-k.png',
+			description:
+				'BK is a true full-stack senior developer covering everything from database through backend and frontend including pipelines and DevOps processes.'
+		},
+		{
+			name: 'Laszlo',
+			role: 'Software Engineer',
+			imgSrc: '/images/team/laszlo.png',
+			description:
+				'Laszlo is an engineering leader who is keen on new technologies and methodologies that support software development. He thrives on creating and optimizing SLDC pipelines, including developer experience, release and testing infrastructure and monitoring solutions.'
+		},
+		{
+			name: 'Szabi',
+			role: 'Engineer',
+			imgSrc: '/images/team/szabi.png',
+			description:
+				"Szabi loved video games for as long as he can remember. After he got his first NES console when he was 4, gaming became a huge part of his life. Szabi grew up collecting NES games and storing them in a shoebox, running around the house like Sonic, and killing imaginary enemies with my imaginary guns like Lara Croft. He's a Generalist, ready to work on any part of a game."
+		},
+		{
+			name: 'Betti',
+			role: 'Recruiter',
+			imgSrc: '/images/team/betti.png',
+			description:
+				'Betti has a remarkable ability to identify exceptional talents. She has worked with global companies including Cloudera and GitLab to assemble winning teams that thrive on creative problem solving. Her emphasis on fostering a culture of innovation and driving efficiency has made a significant impact on the success of the organizations she has worked with.'
+		},
+		{
+			name: 'Andras',
+			role: 'Engineer',
+			imgSrc: '/images/team/andras.png',
+			description:
+				'Ever since his childhood Andras was always entertained by the worlds of books, movies, and video games. He also enjoy creating and building things, figuring out solutions to mind-bending problems. Game development has everything Andras love: fantasy, creativity, ingenuity. Andras mainly enjoy working on Gameplay, that is where he is most experienced.'
+		},
+		{
+			name: 'Reka',
+			role: 'Designer',
+			imgSrc: '/images/team/reka.png',
+			description:
+				'Réka is our designer with a passion for creating visually stunning and user-friendly digital experiences. Réka has loved the world of art since she was young. She studied engineering at university, where she realized that the combination of the two was the perfect profession for her.'
+		},
+		{
+			name: 'Alex',
+			role: 'Engineer',
+			imgSrc: '/images/team/alex.png',
+			description:
+				'Alex is a passionate software engineer, striving for efficiency in his work. His journey began in high school, where he learned programming, and it has led him to experiment with all kinds of coding. He finds joy in developing optimized solutions for backend services and internal tools.'
+		},
+		{
+			name: 'Ishai',
+			role: 'Advisor',
+			imgSrc: '/images/team/ishai.png',
+			description:
+				'On an advisory level Ishai brings to us his extensive work experience in the tech industry. Ishai was the CEO & Founder of Growth Streak, Business Performance at Proteus Games, Business Growth Advisor at Lightricks, CPO at JOB TODAY, Senior Director, Product Management & Business Performance and Director, Product Management & Business Performance at King, Lead Product Manager, Senior Product Manager and Product Manager at Electronic Arts (EA), Co-Founder/Product Manager/Android Developer at CraveEat Mobile, Business Development Manager / Economy Manager at P-Kama, and Business Development Manager at Shidonni.. He also held a Strategic Review and Marketing Intelligence Project at Geoservices.  Ishai Shamir received their MBA from HEC Paris in 2009, specializing in Entrepreneurship. Ishai also obtained a Bachelor of Arts (BA) in Psychology and a Bachelor of Laws (LL.B.) from Tel Aviv University between 2002 and 2006.'
+		}
+	];
+</script>
+
+<svelte:head>
+	<title>Team | Galactic Syndicate</title>
+</svelte:head>
+
+<section
+	class="px-wrapper w-full bg-black bg-[url('/svgs/layout/stars-mobile.svg')] bg-contain bg-no-repeat pt-headerHeightMd text-white md:bg-[url('/svgs/layout/stars.svg')] md:bg-auto"
+>
+	<div
+		class=" mx-auto flex w-full max-w-screen-2xl flex-col items-center justify-start gap-4 py-20 md:gap-20 xl:gap-40"
+	>
+		<h2
+			class="font-aldrich text-2xl uppercase text-blue md:text-4xl xl:text-7xl"
+		>
+			Our team
+		</h2>
+		<ul class="space-y-16">
+			{#each team as m, i}
+				<li>
+					<MemberCard member={m} align={i % 2 ? 'left' : 'right'} />
+				</li>
+			{/each}
+		</ul>
+	</div>
+</section>
