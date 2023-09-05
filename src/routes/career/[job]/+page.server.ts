670542fe761e0665de8bf2737fa0cff945c0b25c
balits <balits.peter.marton@gmail.com>
2023-09-05 16:25:10 +0200

    fix(typo)

--- src/routes/career/[job]/+page.server.ts
+++ src/routes/career/[job]/+page.server.ts
@@ -3,8 +3,10 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = ({ params }) => {
-	const job = jobs.find((j) => (j.slug = params.job));
+	const job = jobs.find((j) => j.slug === params.job);
+
 	if (!job) throw redirect(308, '/career');
+
 	return {
 		job
 	};
