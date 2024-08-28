3936ebcde3efafea787b4fd225713029eb0a0b9e
bollokakos-galactic <akos.bollok@galacticsyn.com>
2024-08-28 17:08:55 +0200

    added fashiongamejam2024 subroute and redirect to x-place site

---
+++ src/routes/fashiongamejam2024/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Fashnion GameJam 2024 | Galactic Fleet</title>
+	<meta name="description" content="Portfolio description" />
+</svelte:head>
+
+<section
+	class="px-wrapper pt-headerHeight min-h-screen bg-black font-aldrich text-2xl"
+>
+	<p>REGISTRATION COMING SOON...</p>
+</section>
