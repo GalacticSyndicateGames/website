50f2d6cf252019273fa8dd4b6cb694e847d809ee
Mihály Köles <mihaly.koles@galacticsyn.com>
2024-09-23 17:50:37 +0200

    added first version of the landing page

---
+++ src/routes/fashiongamejam2024-page/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Landing - Fashnion GameJam 2024 | Galactic Fleet</title>
+	<meta name="description" content="Portfolio description" />
+</svelte:head>
+
+<section
+	class="px-wrapper pt-headerHeight min-h-screen bg-black font-aldrich text-2xl"
+>
+	<img src="/images/fashiongamejam2024/poster.jpg" alt="Landing" />
+</section>
