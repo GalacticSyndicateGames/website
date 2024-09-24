3395b811e96fc1405b98049bb1fbc9e92cb3e600
nyuwec <nyuwec@gmail.com>
2024-09-24 10:01:22 +0200

    move it to the redirect page

--- src/routes/fashiongamejam2024/+page.svelte
+++ src/routes/fashiongamejam2024/+page.svelte
@@ -1,10 +1,18 @@
 <svelte:head>
-	<title>Fashnion GameJam 2024 | Galactic Fleet</title>
+	<title>Fashion GameJam 2024 | Galactic Fleet</title>
 	<meta name="description" content="Portfolio description" />
 </svelte:head>
 
 <section
 	class="px-wrapper pt-headerHeight min-h-screen bg-black font-aldrich text-2xl"
 >
-	<p>REGISTRATION COMING SOON...</p>
+	<a
+		href="https://docs.google.com/forms/d/e/1FAIpQLSdw0oTptRqO2I94u7S1PcRsYDvfvqj5LYqgaEvONtquTNzstQ/viewform"
+	>
+		<img
+			src="/images/fashiongamejam2024/poster.jpg"
+			alt="Landing"
+			usemap="#image-map"
+		/>
+	</a>
 </section>
